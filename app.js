0a3e7ced725c96cbb1087bb7531ac462cd4903a1
Jose Browne <josebrowne@gmail.com>
2022-12-03 15:32:38 -0500

    add endpoint

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const port = process.env.PORT || 3001;
 
 app.get("/", (req, res) => res.type('html').send(html));
 
+app.post("/save-roam-sr-data", (req, res) => {
+  console.log(req.body);
+  res.send("OK");
+});
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 
