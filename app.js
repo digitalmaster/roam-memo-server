eb34dd5cf49018706626be9ce290e2e57e832032
Jose Browne <josebrowne@gmail.com>
2022-12-28 15:33:05 -0400

    Iteration fixes

--- app.js
+++ app.js
@@ -1,41 +1,61 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 const bodyParser = require('body-parser')
+const axios = require('axios');
+var timeout = require('connect-timeout');
 
-app.get("/", (req, res) => res.type('html').send(html));
+// To prevent timout when node is waking up from sleep state
+app.use(timeout('2hrs'))
+const haltOnTimedout = (req, res, next) => {
+  if (!req.timedout) next()
+}
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }))
 
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json({limit: '50mb'}))
 
 // Set CORS headers
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://roamresearch.com");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
   next();
 });
 
+app.use(haltOnTimedout)
+
+app.get("/", (req, res) => res.type('html').send(html));
+
 app.post("/save-roam-sr-data", async (req, res) => {
   const { data, graphName, } = req.body;
-  const headers = req.headers;
 
   try {
-    await fetch(`https://api.roamresearch.com/api/graph/${graphName}/write`, {
+    const response = await fetch(`https://api.roamresearch.com/api/graph/${graphName}/write`, {
       method: 'POST',
       body: JSON.stringify(data),
       headers: {
         'Authorization': req.headers.authorization,
         'Content-Type': 'application/json',
-      }
+      },
     });
 
-    res.send("OK");
+    const responseCode = response.status;
+
+    if(responseCode !== 200) {
+      const responseText = await response.text();
+      const error = new Error(`Error Saving Roam Data: ${responseCode}`);
+      error.status = responseCode;
+      error.message = responseText;
+
+      throw error;
+    }
+
+    res.send('Success');
   } catch (error) {
     console.log('Error Saving Roam Data', error);
-    res.status(error.status).send(error.message)
+    res.status(error.status || 500).send(error.message)
   }
 
 });
@@ -88,7 +108,7 @@ const html = `
   </head>
   <body>
     <section>
-      Hello from Render!
+      Hello from Roam Memo 😊!
     </section>
   </body>
 </html>
